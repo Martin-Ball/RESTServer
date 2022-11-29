40bbbcc2e2627c1c8a457421792e7b0e3b181110
Martin Ballester <mballester@q4tech.com>
2022-11-28 23:26:16 -0300

    products and categories endpoint created

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,5 +1,5 @@
+const { Usuario, Categoria, Producto } = require('../models')
 const Role = require('../models/role')
-const Usuario = require('../models/usuario')
 
 const esRoleValido = async( rol = '' ) => {
     const existeRol = await Role.findOne({ rol } )
@@ -23,6 +23,22 @@ const existeUsuarioPorID = async( id ) => {
     }
 }
 
+const existeCategoriaPorId = async( id ) => {
+    // Verificar si el correo existe
+    const existeCategoria = await Categoria.findById(id);
+    if ( !existeCategoria ) {
+        throw new Error(`El id no existe ${ id }`);
+    }
+}
+
+const existeProductoPorId = async( id ) => {
+    // Verificar si el correo existe
+    const existeProducto = await Producto.findById(id);
+    if ( !existeProducto ) {
+        throw new Error(`El id no existe ${ id }`);
+    }
+}
+
 module.exports = {
-    esRoleValido, emailExiste, existeUsuarioPorID
+    esRoleValido, emailExiste, existeUsuarioPorID, existeCategoriaPorId, existeProductoPorId
 }
