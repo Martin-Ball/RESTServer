2dd05e9339173749136b7c9d7e2376d8767c0b83
Martin Ballester <mballester@q4tech.com>
2022-11-29 12:17:49 -0300

    busquedas

--- controllers/buscar.js
+++ controllers/buscar.js
@@ -1,6 +1,6 @@
 const { response } = require("express");
 const { ObjectId } = require('mongoose').Types
-const { Usuario, Categoria, Producto } = require('../models')
+const { Usuario, Categoria, Producto, Role } = require('../models')
 
 const coleccionesPermitidas = [
     'usuarios',
@@ -39,6 +39,94 @@ const buscarUsuarios = async(termino = '', res = response) =>{
 
 }
 
+const buscarCategoria = async(termino = '', res = response) =>{
+
+    const esMongoID = ObjectId.isValid(termino)
+
+    if(esMongoID){
+        const categoria = await Categoria.findById(termino)
+        return res.status(200).json({
+            results: (categoria) ? [ categoria ] : [] //si el usuario existe lo mando sino devuelvo un array vacio
+        })
+    }
+
+    const regex = new RegExp( termino, 'i') //Busqueda no sensible a mayusculas, aparece todo lo que tenga relacion al termino
+
+    const categoria = await Categoria.find({ 
+        $and: [{ estado: true }, { nombre : regex }] //El correo o el nombre coincida con la expresion regular 
+    })
+
+    const cantidad = await Categoria.count({ 
+        $and: [{ estado: true }, { nombre : regex }]
+    })
+
+    res.json({
+        cantidad: cantidad,
+        results: categoria
+    })
+
+}
+
+const buscarProducto = async(termino = '', res = response) =>{
+
+    const esMongoID = ObjectId.isValid(termino)
+
+    if(esMongoID){
+        const producto = await Producto.findById(termino).populate('categoria', 'nombre')
+        return res.status(200).json({
+            results: (producto) ? [ producto ] : [] //si el usuario existe lo mando sino devuelvo un array vacio
+        })
+    }
+
+    const regex = new RegExp( termino, 'i') //Busqueda no sensible a mayusculas, aparece todo lo que tenga relacion al termino
+
+    const producto = await Producto.find({ 
+        $or: [{ nombre : regex }],
+        $and: [{ estado: true }] //El correo o el nombre coincida con la expresion regular 
+    }).populate('categoria', 'nombre')
+
+    const cantidad = await Producto.count({ 
+        $or: [{ nombre : regex }],
+        $and: [{ estado: true }] //El correo o el nombre coincida con la expresion regular 
+    })
+
+    res.json({
+        cantidad: cantidad,
+        results: producto
+    })
+
+}
+
+const buscarRol = async(termino = '', res = response) =>{
+
+    const esMongoID = ObjectId.isValid(termino)
+
+    if(esMongoID){
+        const rol = await Role.findById(termino)
+        return res.status(200).json({
+            results: (rol) ? [ rol ] : [] //si el usuario existe lo mando sino devuelvo un array vacio
+        })
+    }
+
+    const regex = new RegExp( termino, 'i') //Busqueda no sensible a mayusculas, aparece todo lo que tenga relacion al termino
+
+    const rol = await Role.find({ 
+        $or: [{ nombre : regex }],
+        $and: [{ estado: true }] //El correo o el nombre coincida con la expresion regular 
+    })
+
+    const cantidad = await Role.count({ 
+        $or: [{ nombre : regex }],
+        $and: [{ estado: true }] //El correo o el nombre coincida con la expresion regular 
+    })
+
+    res.json({
+        cantidad: cantidad,
+        results: rol
+    })
+
+}
+
 const buscar = (req, res = response) => {
 
     const { coleccion, termino } = req.params
@@ -54,13 +142,13 @@ const buscar = (req, res = response) => {
             buscarUsuarios(termino, res)
         break
         case 'categoria':
-
+            buscarCategoria(termino, res)
         break
         case 'productos':
-
+            buscarProducto(termino, res)
         break
         case 'roles':
-
+            buscarRol(termino, res)
         break
         default:
             res.status(500).json({
