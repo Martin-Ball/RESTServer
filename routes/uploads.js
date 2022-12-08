462620ad1f25fbacda18210064dc417094fd1f45
Martin Ballester <mballester@q4tech.com>
2022-12-08 18:56:24 -0300

    get images uploads

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,7 +1,7 @@
 const { Router } = require('express')
 const { check } = require('express-validator')
 const { validarCampos } = require('../middlewares/validar-campos')
-const { cargarArchivo, actualizarImagen } = require('../controllers/uploads')
+const { cargarArchivo, actualizarImagen, mostrarImagen } = require('../controllers/uploads')
 const { coleccionesPermitidas } = require('../helpers/db-validators')
 const { validarArchivoSubir } = require('../middlewares/validar-archivo')
 
@@ -15,5 +15,11 @@ router.put('/:coleccion/:id', [
     validarArchivoSubir,
     validarCampos
 ], actualizarImagen)
+
+router.get('/:coleccion/:id', [
+    check('id', 'El id debe ser de mongo').isMongoId(),
+    check('coleccion').custom( c => coleccionesPermitidas( c, ['usuarios', 'productos'])),
+    validarCampos
+], mostrarImagen)
 
 module.exports = router
